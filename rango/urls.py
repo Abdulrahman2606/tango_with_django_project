1ae697e7965a60547379d194ec87fd2645e31323
Abdulrahman Saleh <2502556s@student.gla.ac.uk>
2023-02-04 17:23:16 +0000

    Completed chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -11,7 +11,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
