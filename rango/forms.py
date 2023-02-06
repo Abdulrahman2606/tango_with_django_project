17d2b457c73b7e2f2a6e3a95540ec9e197f0cccd
Abdulrahman Saleh <2502556s@student.gla.ac.uk>
2023-02-06 21:12:48 +0000

    Completed chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -6,7 +6,8 @@ Created on Sat Feb  4 18:28:06 2023
 """
 
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.Name_Max_Length,
@@ -39,5 +40,17 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+        
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
         
 
